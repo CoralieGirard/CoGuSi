323d2bff1a5eaacf91f41cfdb0cb0404cbec6316
Guillaume Gauvin <57801844+Nivuag@users.noreply.github.com>
2019-11-28 15:46:24 -0500

    Added Albums and removed Other useless classes
    Removed Thread and posts

---
+++ proj1/displaythread.php
@@ -0,0 +1,28 @@
+<?php
+  session_start();
+
+  if(!isset($_GET["threadTitle"])){
+    header("Location: error.php?ErrorMSG=Bad%20Request!");
+    die();
+  }
+
+  $title=$_GET["threadTitle"];
+  $id = $_GET["threadID"];
+
+  if(isset($_COOKIE["$id"]))
+  {
+    setcookie("$id", ++$_COOKIE["$id"], time()+ (3600*24*365));
+  }
+  else
+  {
+    setcookie("$id", 0, time()+ (3600*24*365));
+  }
+
+  $content = array();
+  array_push($content, "postlistview.php");
+  array_push($content, "postcreateview.php");
+
+  require_once __DIR__ . "/HTML/masterpage.php";
+
+
+?>
