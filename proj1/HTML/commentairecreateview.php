bd2087db32ab7a4705714563cd0cfe93c788172d
CoralieGirard <58181974+CoralieGirard@users.noreply.github.com>
2019-12-02 12:23:14 -0500

    Add files via upload

---
+++ proj1/HTML/commentairecreateview.php
@@ -0,0 +1,21 @@
+<div class="container">
+
+  <form method = "post" action = "DOMAINLOGIC/addCommentaire.dom.php">
+    <input type="hidden" name="idType" id="idType" value="<?php echo $_GET["idType"] ?>" required>
+    <input type="hidden" name="Type" id="Type" value="<?php echo $_GET["Type"] ?>" required>
+
+    <div class="form-group">
+            <textarea class="form-control" rows="5" name="contenu" id="contenu" placeholder="Got something to say, punk?" required></textarea>
+            <div class="valid-feedback">Valid.</div>
+            <div class="invalid-feedback">Please fill out this field.</div>
+    </div>
+    
+    <div class="btn-group">
+      <button class="btn btn-success btn-lg mb-4" type="submit">Post that stuff!</button>
+      <button class="btn btn-warning btn-lg mb-4" type="reset">Actually, nevermind!</button>
+    </div>
+
+    
+  </form>
+
+</div>
