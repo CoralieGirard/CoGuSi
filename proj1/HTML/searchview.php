dd85ef7ce0b68c4e8e5184b5e4f7ca596b719868
EDtruk <58182635+EDtruk@users.noreply.github.com>
2019-12-02 11:43:44 -0500

    Update searchview.php

--- proj1/HTML/searchview.php
+++ proj1/HTML/searchview.php
@@ -1,11 +1,14 @@
+
 <?php
-    include "CLASSES/THREAD/thread.php";
-    $thread_list = Thread::search_thread($_GET["search"]);
+//  Fait par Simon
+    include "./USER/userTDG.php";
+    $userList = getByUsername($_GET["search"]);
 ?>
 
 <h1 class="my-4">Search results:</h1>
 <?php
-  foreach($thread_list as $thread){
-    $thread->display_thread();
+ foreach($userList as $user){
+
+    }
   }
 ?>
