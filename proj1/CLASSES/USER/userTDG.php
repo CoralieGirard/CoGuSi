319e8481b811383d796338e67e45d7f1f0d23ced
EDtruk <58182635+EDtruk@users.noreply.github.com>
2019-12-03 12:19:42 -0500

    Update userTDG.php

--- proj1/CLASSES/USER/userTDG.php
+++ proj1/CLASSES/USER/userTDG.php
@@ -19,7 +19,7 @@ class UserTDG extends DBAO{
 
     public static function getInstance(){
         if(is_null(self::$_instance)){
-            self::$_instance = UserTDG::getInstance();
+            self::$_instance = new UserTDG();
         }
         return self::$_instance;
     }
