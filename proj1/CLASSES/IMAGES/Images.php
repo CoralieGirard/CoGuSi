6e41822ff9a11f3d8f4c634c0db03b5bfb1950af
CoralieGirard <58181974+CoralieGirard@users.noreply.github.com>
2019-12-05 16:29:23 -0500

    Add files via upload

---
+++ proj1/CLASSES/IMAGES/Images.php
@@ -0,0 +1,199 @@
+<?php
+
+include_once __DIR__ . "/ImagesTDG.php";
+include_once __DIR__ . "/../Commentaires/Commentaires.php";
+date_default_timezone_set("America/New_York");
+
+class Images{
+
+    private $idImage;
+    private $URL;
+    private $idAlbum;
+    private $Description;
+    private $DateCreation;
+    private $Commentaires;
+
+    public function __construct(){
+      $this->Commentaires = array();
+    }
+
+    //getters
+    public function getIdImage(){
+        return $this->idImage;
+    }
+
+    public function getURL(){
+        return $this->URL;
+    }
+
+    public function getIdAlbum(){
+        return $this->idAlbum;
+    }
+
+    public function getDescription(){
+        return $this->Description;
+    }
+
+    public function getDateCreation(){
+        return $this->DateCreation;
+    }
+
+    //setters
+    public function setIdImage($idImage){
+        $this->idImage = $idImage;
+    }
+
+    public function setURL($URL){
+        $this->URL = $URL;
+    }
+
+    public function setIdAlbum($idAlbum){
+        $this->idAlbum = $idAlbum;
+    }
+
+    public function setDescription($description){
+        $this->Description = $description;
+    }
+
+    public function setDateCreation($dateCreation){
+        $this->DateCreation = $dateCreation;
+    }
+
+    /*
+        Quality of Life methods (Dans la langue de shakespear (ou QOLM pour les intimes))
+    */
+    public function loadImageById($idImage){
+        $TDG = ImageTDG::getInstance();
+        $res = $TDG->getByIdImage($idImage);
+
+        if(!$res){
+            return false;
+        }
+
+        $this->idImage = $res["idImage"];
+        $this->URL = $res["URL"];
+        $this->idAlbum = $res["idAlbum"];
+        $this->Description = $res["Description"];
+        $this->DateCreation = $res["DateCreation"];
+
+        return true;
+    }
+
+    public function loadImageByIdAlbum($idAlbum){
+        $TDG = ImagesTDG::getInstance();
+        $res = $TDG->getByIdAlbum($idAlbum);
+
+        if(!$res){
+            return false;
+        }
+
+        $this->idImage = $res["idImage"];
+        $this->URL = $res["URL"];
+        $this->idAlbum = $res["idAlbum"];
+        $this->Description = $res["Description"];
+        $this->DateCreation = $res["DateCreation"];
+
+        return true;
+    }
+
+    public function loadImageByDateCreation($date){
+        $TDG = ImagesTDG::getInstance();
+        $res = $TDG->getByDateCreation($date);
+
+        if(!$res){
+            return false;
+        }
+
+        $this->idImage = $res["idImage"];
+        $this->URL = $res["URL"];
+        $this->idAlbum = $res["idAlbum"];
+        $this->Description = $res["Description"];
+        $this->DateCreation = $res["DateCreation"];
+
+        return true;
+    }
+
+    public function addImage($URL, $idAlbum, $description){
+        $TDG = ImagesTDG::getInstance();
+        $dateCreation = date("Y-m-d H:i:s");
+        $res = $TDG->addImage($URL, $idAlbum, $description, $dateCreation);
+        $TDG = null;
+        if(!$res)
+        {
+            return $res;
+        }
+        return $res;
+    }
+
+    public function displayImage(){ /////////////////////////////////////////////////////////////////////////////////////////////////
+        $title = $this->title;
+        $id = $this->id;
+        echo "<div class='card bg-dark mb-4'>";
+        echo "<div class='card-header text-left '><a href='displaythread.php?threadID=$id&threadTitle=$title'><h5>$title</h5></a>";
+        echo "</div>";
+        echo "</div>";
+    }
+
+    /*
+    Post related functions
+    */
+    public function loadCommentaires(){
+        $res = Commentaires::createCommentaireList($this->idImage, "Image");
+
+        if(!$res)
+        {
+            return false;
+        }
+
+        $this->Commentaires = $res;
+    }
+
+    public function displayCommentaire(){
+        if(empty($this->Commentaires)){
+            $this->loadCommentaires();
+        }
+
+        if(empty($this->Commentaires))
+        {
+            echo "<h3 class='mb-4'>Aucun commentaire</h3>";
+        }
+        else{
+
+            foreach($this->Commentaires as $Commentaires => $Commentaire){
+                $Commentaire >display();
+              }
+        }
+    }
+
+    /*
+    STATIC FUNCTIONS
+    */
+    private static function listAllImages(){
+        $TDG = ImagesTDG::getInstance();
+        $res = $TDG->getAllImages();
+        $TDG = null;
+        if(!$res)
+        {
+          return $res;
+        }
+        return $res;
+    }
+
+    public static function createImageList(){
+        $TDGRes = Thread::listAllImages();
+        $imagesList = array();
+
+        foreach($TDGRes as $r){
+            $image = new Images();
+            $image->setIdImage($r["idImage"]);
+            $image->setURL($r["URL"]);
+            $image->setIdAlbum($r["idAlbum"]);
+            $image->setDescription($r["Description"]);
+            $image->setDateCreatetion($r["DateCreation"]);
+            array_push($imagesList, $image);
+        }
+
+        return $imagesList;
+    }
+
+}
---
+++ proj1/CLASSES/IMAGES/ImagesTDG.php
@@ -0,0 +1,179 @@
+<?php
+
+include_once __DIR__ . "/../../UTILS/connector.php";
+
+class ImagesTDG extends DBAO{
+
+    private $tableName;
+    private static $instance =null;
+
+    public function __construct(){
+        Parent::__construct();
+        $this->tableName = "Image";
+    }
+
+    public static function getInstance()
+    {
+        if(self::$instance == null)
+        {
+            self::$instance = new ImagesTDG();
+        }
+        
+        return self::$instance;
+    }
+
+    //create table
+    public function createTable(){
+
+        try{
+            $conn = $this->connect();
+            $query = "CREATE TABLE IF NOT EXISTS ". $this->tableName ." (idImage INTEGER(10) AUTO_INCREMENT PRIMARY KEY,
+            URL VARCHAR(1000),
+            idAlbum INT,
+            Description VARCHAR(1000),
+            DateCreation VARCHAR(30))";
+            $stmt = $conn->prepare($query);
+            $stmt->execute();
+            $resp = true;
+        }
+
+        //error catch
+        catch(PDOException $e)
+        {
+            $resp = false;
+        }
+        //fermeture de connection PDO
+        $conn = null;
+        return $resp;
+    }
+
+
+    //drop table
+    public function dropTable(){
+
+        try{
+            $conn = $this->connect();
+            $query = "DROP TABLE ". $this->tableName;
+            $stmt = $conn->prepare($query);
+            $stmt->execute();
+            $resp = true;
+        }
+
+        //error catch
+        catch(PDOException $e)
+        {
+            $resp = false;
+        }
+        //fermeture de connection PDO
+        $conn = null;
+        return $resp;
+    }
+
+    public function getAllImages(){
+
+        try{
+            $conn = $this->connect();
+            $query = "SELECT * FROM ". $this->tableName;
+            $stmt = $conn->prepare($query);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetchAll();
+        }
+
+        //error catch
+        catch(PDOException $e)
+        {
+            $result = false;
+        }
+        //fermeture de connection PDO
+        $conn = null;
+        return $result;
+    }
+
+
+    public function getByIdImage($idImage){
+
+        try{
+            $conn = $this->connect();
+            $query = "SELECT * FROM ". $this->tableName ." WHERE idImage=:id";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':id', $idImage);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetch();
+        }
+
+        catch(PDOException $e)
+        {
+            return false;
+        }
+        //fermeture de connection PDO
+        $conn = null;
+        return $result;
+    }
+
+    public function getByIdAlbum($idAlbum){
+
+        try{
+            $conn = $this->connect();
+            $query = "SELECT * FROM ". $this->tableName ." WHERE idAlbum=:idAlbum";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':idAlbum', $idAlbum);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetch();
+        }
+
+        catch(PDOException $e)
+        {
+            return false;
+        }
+        //fermeture de connection PDO
+        $conn = null;
+        return $result;
+    }
+
+    public function getByDateCreation($dateCreation){
+
+        try{
+            $conn = $this->connect();
+            $query = "SELECT * FROM ". $this->tableName ." WHERE DateCreation=:date";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':date', $dateCreation);
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetch();
+        }
+
+        catch(PDOException $e)
+        {
+            return false;
+        }
+        //fermeture de connection PDO
+        $conn = null;
+        return $result;
+    }
+
+    public function addImage($URL, $idAlbum, $description, $DateCreation){
+
+        try{
+            $conn = $this->connect();
+            $tableName = $this->tableName;
+            $query = "INSERT INTO $tableName (URL, idAlbum, Description, DateCreation) VALUES (:URL, :idAlbum, :description, :date)";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(':URL', $URL);
+            $stmt->bindParam(':idAlbum', $idAlbum);
+            $stmt->bindParam(':description', $description);
+            $stmt->bindParam(':date', $DateCreation);
+            $stmt->execute();
+            $res = true;
+        }
+        catch(PDOException $e)
+        {
+            $res = false;
+        }
+        //fermeture de connection PDO
+        $conn = null;
+        return $res;
+    }
+}
