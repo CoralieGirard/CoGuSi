8f23cef7ddb15203323eab77e58157a384172443
CoralieGirard <58181974+CoralieGirard@users.noreply.github.com>
2019-12-11 14:31:50 -0500

    Add files via upload

---
+++ proj1/DOMAINLOGIC/deleteAlbum.php
@@ -0,0 +1,29 @@
+<?php
+    include __DIR__ . "/../CLASSES/ALBUMS/Album.php";
+    include __DIR__ . "/../UTILS/sessionhandler.php";
+
+    session_start();
+
+    if(!validateSession()){
+        header("Location: ../error.php?ErrorMSG=Not%20logged%20in!");
+        die();
+    }
+
+    if(!isset($_GET["idAlbum"])){
+        header("Location: ../error.php?ErrorMSG=Bad%20Requests!");
+        die();
+    }
+
+    $album = new Album();
+    $album->loadAlbum($_GET["idAlbum"]);//////////////////////////////////////
+
+    if(!$album->getProprietaire() == $_SESSION["userID"]){
+        header("Location: ../error.php?ErrorMSG=Bad%20Requests!");
+        die();
+    }
+    
+    $album->delete();
+
+    header("Location: ../billboard.php");
+    die();
+?>
