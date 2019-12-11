25e25ea3d1faea2d5c52adede28c10d8f1c9ed55
EDtruk <58182635+EDtruk@users.noreply.github.com>
2019-12-11 14:47:37 -0500

    Update navigationmodule.php

--- proj1/HTML/navigationmodule.php
+++ proj1/HTML/navigationmodule.php
@@ -39,6 +39,9 @@
             <li class="nav-item">
                 <a class="nav-link" href="billboard.php">MY ALBUMS</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="billboard.php">ALL ALBUMS</a>
+                </li>
            
         </ul>
 
