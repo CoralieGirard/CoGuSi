c1ce7f59cd050eecb2e486573c48c9691337ecd8
CoralieGirard <58181974+CoralieGirard@users.noreply.github.com>
2019-12-12 16:39:49 -0500

    Add files via upload

--- proj1/HTML/albumcreationview.php
+++ proj1/HTML/albumcreationview.php
@@ -1,8 +1,9 @@
 <div class="container border">
-  <h3 class="my-4">Create album </h3>
+  <h3 class="my-4">Créer album </h3>
   <form method = "post" action = "./DOMAINLOGIC/albumcreate.dom.php">
       <div class="form-group">
-          <input type="text" class="form-control" name="albumcreation" id="albumcreation" placeholder="Title" required><br>
+          <input type="text" class="form-control" name="titre" id="titre" placeholder="Titre" required><br>
+          <input type="text" class="form-control" name="description" id="description" placeholder="Description" required><br>
           <div class="valid-feedback">Valid.</div>
       </div>
       <button class="btn btn-success btn-lg mb-4" type="submit">Create Album</button>
--- proj1/HTML/myAlbumListView.php
+++ proj1/HTML/myAlbumListView.php
@@ -1,11 +1,11 @@
 <?php
     include "CLASSES/ALBUMS/Album.php";
-    $album_list = Album::createAlbumList();/////////////////////////////
+    $album_list = Album::createAlbumList();
 ?>
 
 <h3 class="my-4">My albums</h3>
 <?php
   foreach($album_list as $album){
-    $album->displayAlbum();////////////////
+    $album->displayMyAlbum();
   }
 ?>
