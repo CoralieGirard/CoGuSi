05f75c3035217ab780f609d6b7c84b0e01760e48
EDtruk <58182635+EDtruk@users.noreply.github.com>
2019-12-19 16:57:20 -0500

    Add files via upload

--- proj1/HTML/searchview.php
+++ proj1/HTML/searchview.php
@@ -1,31 +1,32 @@
 
 <?php
 //  Fait par Simon
-    include "./USER/userTDG.php";
-    include "./ALBUMS/albumTDG.php";
-    include "./IMAGES/imagesTDG.php";
-    $userList = getUsernameByName($_GET["search"]);
-    $albumList = getAlbumByName($_GET["search"]);
-    $imageList = getImageByName($_GET["search"]);
+    include_once "./CLASSES/USER/user.php";
+    include_once "./CLASSES/ALBUMS/Album.php";
+    include_once "./CLASSES/IMAGES/Images.php";
+    $userList = user::getByName($_GET["search"]);
+    $albumList = Album::getByName($_GET["search"]);
+    $imageList = images::getByName($_GET["search"]);
 ?>
 
-<h1 class="my-4">Search results (users):</h1>
+<h1 class="my-4">Search results (Utilisateurs):</h1>
 <?php
  foreach($userList as $user){
-
+    $user->displayUser();
   }
 ?>
 
 <h1 class="my-4">Search results (Albums):</h1>
 <?php
  foreach($albumList as $album){
-
+   $album->displayAlbum();
   }
 ?>
 
 <h1 class="my-4">Search results (Images):</h1>
 <?php
+ var_dump($imageList);
  foreach($imageList as $image){
-  
+    $image->displayImage();
   }
 ?>
