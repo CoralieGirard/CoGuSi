e5c004282c09535b773148966dea39a3b8bbe98e
Guillaume Gauvin <57801844+Nivuag@users.noreply.github.com>
2019-12-19 17:03:33 -0500

    Update templateImage.php

--- proj1/HTML/templateImage.php
+++ proj1/HTML/templateImage.php
@@ -1,6 +1,6 @@
 <div class="Container">
     <?php 
-        echo "<img src='$URL' alt='$idAlbum#$idImage'>";
+        echo "<a href='ImageCommentaire.php?idImage=$idImage&Type=image'><img src='$URL' alt='$idAlbum#$idImage'></a>";
     ?>
     <h6><?php echo $Description?></h6>
 </div>
